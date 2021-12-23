c68da20ff792e7ff171e580fa58d25b1236ed330
AdrianaAmaral <adrianafsamaral@gmail.com>
2021-12-23 09:35:31 -0300

    Entity Order

--- src/main/java/com/bella/coelho/entities/Client.java
+++ src/main/java/com/bella/coelho/entities/Client.java
@@ -2,16 +2,20 @@ package com.bella.coelho.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -40,14 +44,18 @@ public class Client implements Serializable {
 	@Getter @Setter private String cep;
 	@Getter @Setter private String telefone;
 	@Getter @Setter private String email;
-	@Getter @Setter private String RG;
+	@Getter @Setter private String rg;
 	
 	@CPF
 	@Column(unique = true)
 	@Getter @Setter private String cpf;
 	
 	@JsonFormat(pattern = "dd/MM/yyyy")
-	@Getter @Setter private LocalDate dataNasc;	
+	@Getter @Setter private LocalDate dataNasc;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	@Getter private List<Order> orders = new ArrayList<>();
 	
 	public Client(Object object, String string, String string2, int i, String string3, String string4, String string5,
 			String string6, String string7, String string8, String string9, String string10, String string11) {
